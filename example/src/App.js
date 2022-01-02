d80101d146aff8ef6ad9ba54d6163936411b2f7e
hrs2203 <hrishabh2203@gmail.com>
2022-01-02 16:15:08 +0530

    working editor, notionCopy v1

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'web_package_test'
+import { NotionCloneEditor } from 'web_package_test'
 import 'web_package_test/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <NotionCloneEditor />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,157 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState } from 'react'
+import { Editor, EditorState, convertToRaw, SelectionState, Modifier, CompositeDecorator } from 'draft-js'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Components: {text}</div>
+export const NotionCloneEditor = ({ isDebug = false }) => {
+  const decorationStratergy = new CompositeDecorator([
+    {
+      strategy: (contentBlock, callback, contentState) => {
+        const text = contentBlock.getText();
+        const len = text.length;
+        let i = 0;
+        while (i < len) {
+          let j = i;
+          while ((text[j] !== " ") && (j < len)) {
+            j += 1;
+          }
+          callback(i, j);
+          i = j + 1;
+        }
+      },
+      component: (props) => {
+        const entityKey = props.entityKey;
+        const entData = entityKey === null ? {} : customEditor.getCurrentContent().getEntity(entityKey).getData();
+        const defaultStyle = {
+          "fontStyle": "normal", "fontWeight": "normal"
+        }
+        const customStyle = entityKey === null ? defaultStyle : {
+          ...defaultStyle,
+          "fontStyle": entData["i"] === true ? "italic" : defaultStyle["fontStyle"],
+          "fontWeight": entData["b"] === true ? "bold" : defaultStyle["fontWeight"],
+          "textDecoration": entData["u"] === true ? "underline" : "inherited"
+        }
+        return <span style={customStyle}>{props.children}</span>
+      },
+    }
+  ])
+
+  const [customEditor, setCustomEditor] = useState(() => EditorState.createEmpty(decorationStratergy));
+
+  const handelChange = (newEditorState) => {
+    setCustomEditor(newEditorState);
+  }
+
+  const customStyle = {
+    "border": "1px solid black",
+    "margin": "10px",
+    "padding": "3px"
+  }
+
+  const selectUpdate = (changeKeyValues) => {
+    let contentState = customEditor.getCurrentContent();
+    const selectionState = customEditor.getSelection();
+    const startKey = selectionState.getStartKey();
+    const endKey = selectionState.getEndKey();
+    if (startKey !== endKey) {
+      alert("multiple line select not allowed for the momment")
+    }
+    else {
+      const contentBlock = contentState.getBlockForKey(startKey);
+      let startIndex = selectionState.getStartOffset();
+      let endIndex = selectionState.getEndOffset();
+      if (selectionState.isCollapsed()) {
+        console.log("nothing selected");
+      } else {
+        const text = contentBlock.getText()
+        const textLength = text.length;
+        // both indexes are inclusive
+        while ((startIndex > 0) && (text[startIndex - 1] !== " ")) {
+          startIndex -= 1;
+        }
+        while ((endIndex < textLength - 1) && (text[endIndex] !== " ")) {
+          endIndex += 1;
+        }
+        endIndex = (endIndex === textLength - 1) ? endIndex : (endIndex - 1);
+
+        while (startIndex < endIndex) {
+          let j = startIndex;
+          while ((j < endIndex) && (text[j] !== " ")) {
+            j += 1;
+          }
+          j = (j === endIndex) ? j : (j - 1);
+          console.log(text.slice(startIndex, j + 1), startIndex, j, j - startIndex + 1);
+          // ========= operations ===========
+
+          const entityKey = contentBlock.getEntityAt(startIndex);
+          if (entityKey === null) {
+            // create a new one
+            const newSelection = new SelectionState({
+              anchorKey: startKey,
+              focusKey: startKey,
+              anchorOffset: startIndex,
+              focusOffset: j + 1,
+              isBackward: false,
+            });
+            contentState = contentState.createEntity("TOKEN", "MUTABLE", changeKeyValues)
+            const lastCreatedKey = contentState.getLastCreatedEntityKey();
+            contentState = Modifier.applyEntity(contentState, newSelection, lastCreatedKey);
+          } else {
+            // update existing
+            const tempEntity = contentState.getEntity(entityKey);
+            const newData = { ...tempEntity.getData(), ...changeKeyValues }
+            contentState = contentState.replaceEntityData(entityKey, newData);
+          }
+          // ================================
+          startIndex = j + 2;
+        }
+        const newEditorState = EditorState.createWithContent(contentState, decorationStratergy);
+        setCustomEditor(newEditorState);
+      }
+
+    }
+  }
+
+  return (
+    <div style={customStyle}>
+      <div>
+        <button
+          style={{ "paddingLeft": "8px", "paddingRight": "8px", "marginRight": "3px" }}
+          onClick={() => { selectUpdate({ "b": true }) }}
+        ><b>b</b></button>
+        <button
+          style={{ "paddingLeft": "8px", "paddingRight": "8px", "marginRight": "3px" }}
+          onClick={() => { selectUpdate({ "i": true }) }}
+        ><i>i</i></button>
+        <button
+          style={{ "paddingLeft": "8px", "paddingRight": "8px", "marginRight": "3px" }}
+          onClick={() => { selectUpdate({ "u": true }) }}
+        ><u>u</u></button>
+        <button
+          style={{ "paddingLeft": "8px", "paddingRight": "8px", "marginRight": "3px" }}
+          onClick={() => { selectUpdate({ "b": false }) }}
+        ><b>un b</b></button>
+        <button
+          style={{ "paddingLeft": "8px", "paddingRight": "8px", "marginRight": "3px" }}
+          onClick={() => { selectUpdate({ "i": false }) }}
+        ><i>un i</i></button>
+        <button
+          style={{ "paddingLeft": "8px", "paddingRight": "8px", "marginRight": "3px" }}
+          onClick={() => { selectUpdate({ "u": false }) }}
+        ><u>un u</u></button>
+      </div>
+      <br />
+      <Editor
+        editorState={customEditor}
+        onChange={handelChange}
+      />
+      {
+        isDebug && (
+          <button onClick={() => {
+            console.log(convertToRaw(customEditor.getCurrentContent()));
+          }}>log data</button>
+        )
+      }
+      <br />
+
+    </div>
+  )
 }
